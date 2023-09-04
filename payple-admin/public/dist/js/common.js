1dfee09c4cfe8a067cbfb8b65af0af66881c5ace
gotaeyoon <ty.go.payple.kr>
2023-09-04 15:01:26 +0900

    페이지 확인

--- payple-admin/public/dist/js/common.js
+++ payple-admin/public/dist/js/common.js
@@ -1,240 +1,194 @@
-//open Modal
-function openModal(modalname) {
-    $("." + modalname).addClass('show')
-    setTimeout(function () {
-        $("." + modalname).addClass('fade')
-    }, 200);
-}
-// close Modal
-function closeModal() {
-    let modals = $('.modal')
-    let closeBtn = $('._modal_close_btn')
-    closeBtn.on('click', function () {
-        modals.removeClass('fade')
-        setTimeout(function () {
-            modals.removeClass('show')
-        }, 200);
-    })
-}
-// close dim
-function dimModal() {
-    let modals = $('.modal')
-    modals.on('click', function (e) {
-        if (e.target == this) {
-            modals.removeClass('fade')
-            setTimeout(function () {
-                modals.removeClass('show')
-            }, 200)
-        }
+//Aside UI
+function asideSlideBtn() {
+    let title = $('._title');
+    let slide = $('._slide');
+
+    title.on('click', function () {
+        $(this).next(slide).stop().slideToggle(300);
+        $(this).toggleClass('active').parent().siblings().find(title).removeClass('active');
+        $(this).parent().siblings().find(slide).slideUp(300);
     })
 }
 
-
-//전화번호 hyphen
-function formatPhoneNumber() {
-    $('._input_phone').on('input', function () {
-        this.value = this.value
-            .replace(/[^0-9]/g, '')
-            .replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`);
+//Aside open UI
+function openAsideBtn() {
+    let openAsideBtn = $('._btn_open_depths')
+    openAsideBtn.on('click', function () {
+        $(this).parent('.aside').toggleClass('on')
+        $('.footer').toggleClass('on')
     })
 }
 
-
-// header 버튼
-function toggleAccountInfo() {
-    let accountInfo = $('._header__account_info');
-    let accountBtn = $('._ico_account_btn');
-    accountBtn.on('click', function (e) {
-        e.stopPropagation();
-        accountInfo.toggleClass('show');
-    });
-    $(document).on('click', function (event) {
-        let target = $(event.target);
-        if (!target.is(accountInfo) && !target.is(accountBtn)) {
-            accountInfo.removeClass('show');
-        }
-    });
-}
-
-//selectBox 공통
+//selectBox
 function selectBox() {
     let selectBox = $('._select_box')
     selectBox.on('click', function () {
         $(this).toggleClass('active');
     })
-    selectBox.each(function(){
-        let selectBox = $(this);
-        selectBox.find('.option').on('click', function () {
-            let text = $(this).text();
-            let value = $(this).val();
-            $(this).parent().parent().find('.select_value').text(text).css({'color': '#1F1F1F'}).attr('value', value);
-        });
-    })
 
-    $(document).on('click', function (event) {
-        let target = $(event.target);
-        selectBox.each(function() {
+    selectBox.find('.option').on('click', function () {
+        let text = $(this).text();
+        let value = $(this).val();
+        let selectBoxValue =  $(this).closest('._select_box')
+
+        selectBoxValue.find('.select_value').attr('value', value);
+        selectBoxValue.find('.value_text').text(text).css({'color': '#1F1F1F'});
+    });
+
+    $(document).on('click', function (e) {
+        let target = $(e.target);
+        selectBox.each(function () {
             if (!target.is(this) && !target.closest(this).length) {
                 $(this).removeClass('active');
             }
         });
     });
 
-
 }
 
 
-//입점불가 텍스트
-function HoverTextnotAllow(){
-    $('._result').on('mouseenter',function(){
-        $(this).parent().siblings('._hover_text_not_allowed').css({'display':'block'})
-    })
-    $('._result').on('mouseleave',function(){
-        $(this).parent().siblings('._hover_text_not_allowed').css({'display':'none'})
-    })
-}
-
-//서비스 작성중 텍스트
-function HoverTextService(){
-    $('._service').on('mouseenter',function(){
-        $(this).parent().siblings('._hover_text_service').css({'display':'block'})
-    })
-    $('._service').on('mouseleave',function(){
-        $(this).parent().siblings('._hover_text_service').css({'display':'none'})
-    })
-}
-
-//서비스 정지
-function HoverStopService(){
-    $('._stop').on('mouseenter',function(){
-        $(this).parent().siblings('._hover_stop_service').css({'display':'block'})
-    })
-    $('._stop').on('mouseleave',function(){
-        $(this).parent().siblings('._hover_stop_service').css({'display':'none'})
-    })
-}
-
-
-function checkAll() {
-    $('._check_all').on('click',function(){
-        console.log('hello')
-        // if($("#cbx_chkAll").is(":checked")) $("input[name=chk]").prop("checked", true);
-        // else $("input[name=chk]").prop("checked", false);
-        if($("._check_all").is(":checked")){
-            $("input[name=chk]").prop("checked", true);
-        }else{
-            $("input[name=chk]").prop("checked", false);
-        }
-
-    })
-    $("input[name=chk]").click(function() {
-        let total = $("input[name=chk]").length;
-        let checked = $("input[name=chk]:checked").length;
-
-        if(total != checked) $("#cbx_chkAll").prop("checked", false);
-        else $("._check_all").prop("checked", true);
-    });
-}
-
-//테이블 데이터가 없을경우
-function emptyTableData(){
-    $('.empty_data').parents('._scroll_table').css('tableLayout' , 'auto');
-}
-
-
-// 파일명 커스텀
-function inputFile(){
-    $("._file_input").on('change',function(){
-        var fileName = $(this).val().split('/').pop().split('\\').pop();
-        console.log(fileName);
-        $("._file_name").text(fileName);
-    });
-}
-
-
-// ip 입력시 클릭안됨
-function getValidateIp(){
-    $('._input_ip').on('keypress', function(event) {
-        var inputValue = event.which;
-        if (inputValue != 46 && (inputValue < 48 || inputValue > 57)) {
-            event.preventDefault();
-        }
-    });
-}
-
-
-// header 테블릿 버튼
-function asideButton(){
-    $('._aside_button').on('click',function(){
-        $('.aside').toggleClass('show')
-        $(this).toggleClass('active')
-    })
-}
-
-//달력
-function modalCalender(){
-    const parentElement = $('#datepickerParent');
-    let dateRangePicker = $('input[name=dateRangePicker]');
-    dateRangePicker.daterangepicker({
-        linkedCalendars: true,
-        autoApply : true,
-        "locale": {
-            "format": "YYYY-MM-DD",
-            "separator": " ~ ",
-            "applyLabel": "확인",
-            "cancelLabel": "취소",
-            "fromLabel": "From",
-            "toLabel": "To",
-            "customRangeLabel": "Custom",
-            "weekLabel": "주",
-            "daysOfWeek": ["일", "월", "화", "수", "목", "금", "토"],
-            "monthNames": ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+// dataPickerRange
+function dataRangePicker() {
+    const modalDataPicker = $('#modalDataPicker');
+    let dateRangePicker = $('input[name=modalDataPickerBtn]');
+    dateRangePicker.daterangepicker(
+        {
+            linkedCalendars: true,
+            autoApply: true,
+            "locale": {
+                "format": "YYYY-MM-DD",
+                "separator": " ~ ",
+                "weekLabel": "주",
+                "daysOfWeek": ["일", "월", "화", "수", "목", "금", "토"],
+                "monthNames": ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+            },
+            "startDate": new Date(),
+            "endDate": new Date(),
+            "buttonClasses": "data_range_btn",
+            "applyButtonClasses": "apply_range_btn",
+            "cancelButtonClasses": "cancel_range_btn",
+            "showCustomRangeLabelranges": false,
+            "parentEl": modalDataPicker,
         },
-        "startDate": new Date(),
-        "endDate": new Date(),
-        "buttonClasses": "data_range_btn",
-        "applyButtonClasses": "apply_range_btn",
-        "cancelButtonClasses": "cancel_range_btn",
-        "showCustomRangeLabelranges": false,
-        "parentEl": parentElement,
-    }, function (start, end, label) {
-        console.log('선택된 날짜: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+        function (start, end, label) {
+            console.log('선택된 날짜: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+        });
+    dateRangePicker.on('show.daterangepicker', function (ev, picker) {
+        $('#myModal').modal('show')
     });
     dateRangePicker.on('hide.daterangepicker', function (ev, picker) {
-        let modals = $('.modal')
-        modals.removeClass('fade')
-        setTimeout(function () {
-            modals.removeClass('show')
-        }, 200)
+        $('#myModal').modal('hide')
     });
 
-    dateRangePicker.on('showCalendar.daterangepicker', function (ev, picker) {
-        const trList = $('.daterangepicker').find("tr");
-        trList.each(function() {
-            var offEndsAvailableCount = $(this).find(".off.ends.available").length;
-            if (offEndsAvailableCount === 7) {
-                $(this).hide();
-            }
-        });
+    // Last Month 버튼 클릭 시 지난달 범위 선택
+    $('#lastMonthBtn').on('click', function () {
+        var startDate = moment().subtract(1, 'month').startOf('month');
+        var endDate = moment().subtract(1, 'month').endOf('month');
+        dateRangePicker.data('daterangepicker').setStartDate(startDate);
+        dateRangePicker.data('daterangepicker').setEndDate(endDate);
     });
 
+    // This Month 버튼 클릭 시 이번달 범위 선택
+    $('#thisMonthBtn').click(function () {
+        var startDate = moment().startOf('month');
+        var endDate = moment().endOf('month');
+        dateRangePicker.data('daterangepicker').setStartDate(startDate);
+        dateRangePicker.data('daterangepicker').setEndDate(endDate);
+    });
+
+    // Yesterday 버튼 클릭 시 어제 범위 선택
+    $('#yesterdayBtn').click(function () {
+        var startDate = moment().subtract(1, 'day');
+        var endDate = moment().subtract(1, 'day');
+        dateRangePicker.data('daterangepicker').setStartDate(startDate);
+        dateRangePicker.data('daterangepicker').setEndDate(endDate);
+    });
+
+    // Today 버튼 클릭 시 오늘 범위 선택
+    $('#todayBtn').click(function () {
+        var startDate = moment();
+        var endDate = moment();
+        dateRangePicker.data('daterangepicker').setStartDate(startDate);
+        dateRangePicker.data('daterangepicker').setEndDate(endDate);
+    });
 }
 
 
-// 실행
+//slideModal
+function openSlideModal(){
+    $('._open_slide_modal').on('click',function(){
+        $('.slide_modal_backdrop').addClass('on');
+    })
+}
+function closeSlideModal(){
+    $('._close_slide_modal').on('click',function(){
+        $('._slide_modal_backdrop').removeClass('on');
+    })
+}
+
+//Aside ToolTip
+function asideTooltip(){
+    let links =$('.link');
+    links.mouseover(function() {
+        var hoverTitle = $(this).find('a');
+        var top = hoverTitle[0].getBoundingClientRect().top;
+        var arrow = $(this).find('.arrow_box');
+        arrow.css('top', top + 10 + 'px');
+    });
+}
+
+//mobile Aside Btn
+function asideMobileBtn(){
+    $('._aside_button').on('click',function(){
+        $(this).toggleClass('active');
+        if( $(this).hasClass('active')  ){
+            $('.aside').css({'left' : 0})
+        }else{
+            $('.aside').css({'left' : -100 + '%'})
+        }
+    })
+}
+
+
+
+//달력
+// function calender(){
+//     const parentElement = $('#datepickerParent');
+//     let dateRangePicker = $('input[name=dateRangePicker]');
+//     dateRangePicker.daterangepicker(
+//       ,
+//
+//         function (start, end, label) {
+//         console.log('선택된 날짜: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+//     });
+//     dateRangePicker.on('hide.daterangepicker', function (ev, picker) {
+//         let modals = $('.modal')
+//         modals.removeClass('fade')
+//         setTimeout(function () {
+//             modals.removeClass('show')
+//         }, 200)
+//     });
+//
+//     dateRangePicker.on('showCalendar.daterangepicker', function (ev, picker) {
+//         const trList = $('.daterangepicker').find("tr");
+//         trList.each(function() {
+//             var offEndsAvailableCount = $(this).find(".off.ends.available").length;
+//             if (offEndsAvailableCount === 7) {
+//                 $(this).hide();
+//             }
+//         });
+//     });
+//
+// }
+
+
 $(document).ready(function () {
-    formatPhoneNumber();
-    toggleAccountInfo();
-    closeModal();
-    dimModal();
+    dataRangePicker();
+    asideSlideBtn();
+    openAsideBtn();
     selectBox();
-    HoverTextnotAllow();
-    HoverTextService();
-    HoverStopService();
-    checkAll();
-    emptyTableData();
-    inputFile();
-    getValidateIp();
-    asideButton();
-    modalCalender();
+    openSlideModal();
+    closeSlideModal();
+    asideTooltip();
+    asideMobileBtn();
 })
